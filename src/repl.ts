351bb45552a882bcdfdabfd82383cbcb537e6b78
Hiroki Osame <hiroki.osame@gmail.com>
2022-05-17 17:39:11 -0400

    feat: repl welcome message

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,11 @@
 import repl, { type REPLEval } from 'repl';
 import { transform } from '@esbuild-kit/core-utils';
+import { version } from '../package.json';
+
+// Copied from
+// https://github.com/nodejs/node/blob/v18.2.0/lib/internal/main/repl.js#L37
+console.log(`Welcome to tsx v${version} (Node.js ${process.version}).\n` +
+      'Type ".help" for more information.');
 
 const nodeRepl = repl.start();
 
