4b6cf43879ce82fe3153573801c2145d5d7289b9
Hiroki Osame <hiroki.osame@gmail.com>
2022-05-18 18:23:38 -0400

    refactor: use loader internally

--- src/loader.ts
+++ src/loader.ts
@@ -1,3 +1,6 @@
-import '@esbuild-kit/cjs-loader';
+// Hook require() to transform to CJS
+require('@esbuild-kit/cjs-loader');
 
+// Hook import/import() to transform to ESM
+// Can be used in Node v12 to support dynamic `import()`
 export * from '@esbuild-kit/esm-loader';
