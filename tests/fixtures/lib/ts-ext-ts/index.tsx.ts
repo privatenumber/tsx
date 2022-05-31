393b6b6da774dd7609e34c08b725bcfec50543f0
hiroki osame <hiroki.osame@gmail.com>
2022-05-31 17:28:00 -0400

    test(typescript): verify subextensions work (#28)

---
+++ tests/fixtures/lib/ts-ext-ts/index.tsx.ts
@@ -0,0 +1,27 @@
+import fs from 'node:fs';
+
+console.log(
+	'loaded ts-ext-ts/index.tsx.ts',
+	JSON.stringify({
+		nodePrefix: Boolean(fs),
+		hasDynamicImport: Boolean(import('fs')),
+		dirname: typeof __dirname === 'string',
+		...(() => {
+			let nameInError;
+			try {
+				nameInError();
+			} catch (error) {
+				return {
+					nameInError: error.message.includes('nameInError'),
+					sourceMap: error.stack.includes(':11:5'),
+				};
+			}
+		})(),
+	}),
+);
+
+function valueNumber(value: number) {
+	return value;
+}
+
+export default valueNumber(1234);
--- tests/specs/typescript/ts.ts
+++ tests/specs/typescript/ts.ts
@@ -81,6 +81,34 @@ export default testSuite(async ({ describe }, node: NodeApis) => {
 			});
 		});
 
+		describe('extensionless with subextension', ({ test }) => {
+			const importPath = './lib/ts-ext-ts/index.tsx';
+			const outputSubextension = 'loaded ts-ext-ts/index.tsx.ts';
+			const outputSubextensionEsm = `${outputSubextension} {"nodePrefix":true,"hasDynamicImport":true,"dirname":false,"nameInError":true,"sourceMap":false}`;
+			const outputSubextensionCjs = `${outputSubextension} {"nodePrefix":true,"hasDynamicImport":true,"dirname":true,"nameInError":true,"sourceMap":false}`;
+
+			test('Load', async () => {
+				const nodeProcess = await node.load(importPath);
+				expect(nodeProcess.stdout).toBe(node.isCJS ? outputSubextensionCjs : outputSubextensionEsm);
+				expect(nodeProcess.stderr).toBe('');
+			});
+
+			test('Import', async () => {
+				const nodeProcess = await node.import(importPath);
+
+				expect(nodeProcess.stdout).toBe(`${node.isCJS ? outputSubextensionCjs : outputSubextensionEsm}\n{"default":1234}`);
+				expect(nodeProcess.stderr).toBe('');
+			});
+
+			test('Require', async () => {
+				const nodeProcess = await node.require(importPath);
+
+				// By "require()"ing an ESM file, it forces it to be compiled in a CJS context
+				expect(nodeProcess.stdout).toBe(`${outputSubextensionCjs}\n{"default":1234}`);
+				expect(nodeProcess.stderr).toBe('');
+			});
+		});
+
 		describe('directory', ({ test }) => {
 			const importPath = './lib/ts-ext-ts';
 
