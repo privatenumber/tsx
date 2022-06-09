1e0143bf142471b9e937852c7d54a0bf32ee542c
Hiroki Osame <hiroki.osame@gmail.com>
2022-06-08 23:09:00 -0400

    test: tsconfig paths

--- tests/specs/typescript/tsconfig.ts
+++ tests/specs/typescript/tsconfig.ts
@@ -2,12 +2,42 @@ import { testSuite, expect } from 'manten';
 import type { NodeApis } from '../../utils/tsx';
 
 export default testSuite(async ({ describe }, node: NodeApis) => {
-	describe('tsconfig', ({ test }) => {
+	describe('tsconfig', ({ test, describe }) => {
 		test('jsxFactory & jsxFragmentFactory', async () => {
-			const nodeProcess = await node.load('./tsx.tsx', {
+			const nodeProcess = await node.load('./src/tsx.tsx', {
 				cwd: './tsconfig',
 			});
 			expect(nodeProcess.stdout).toBe('div null hello world\nnull null goodbye world');
+		});
+
+		describe('paths', ({ test }) => {
+			test('resolves baseUrl', async () => {
+				const nodeProcess = await node.load('./src/base-url.ts', {
+					cwd: './tsconfig',
+				});
+				expect(nodeProcess.stdout).toBe('resolved');
+			});
+
+			test('resolves paths exact match', async () => {
+				const nodeProcess = await node.load('./src/paths-exact-match.ts', {
+					cwd: './tsconfig',
+				});
+				expect(nodeProcess.stdout).toBe('resolved');
+			});
+
+			test('resolves paths prefix', async () => {
+				const nodeProcess = await node.load('./src/paths-prefix-match.ts', {
+					cwd: './tsconfig',
+				});
+				expect(nodeProcess.stdout).toBe('resolved');
+			});
+
+			test('resolves paths suffix', async () => {
+				const nodeProcess = await node.load('./src/paths-suffix-match.ts', {
+					cwd: './tsconfig',
+				});
+				expect(nodeProcess.stdout).toBe('resolved');
+			});
 		});
 	});
 });
