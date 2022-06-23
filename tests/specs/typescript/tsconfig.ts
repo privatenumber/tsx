ad9de42a8c27673e1841d8aa6e36202b076f917c
Hiroki Osame <hiroki.osame@gmail.com>
2022-06-23 14:25:47 -0400

    style: lintfix

--- tests/specs/typescript/tsconfig.ts
+++ tests/specs/typescript/tsconfig.ts
@@ -3,41 +3,41 @@ import type { NodeApis } from '../../utils/tsx';
 
 export default testSuite(async ({ describe }, node: NodeApis) => {
 	describe('tsconfig', ({ test, describe }) => {
-		// test('jsxFactory & jsxFragmentFactory', async () => {
-		// 	const nodeProcess = await node.load('./src/tsx.tsx', {
-		// 		cwd: './tsconfig',
-		// 	});
-		// 	expect(nodeProcess.stdout).toBe('div null hello world\nnull null goodbye world');
-		// });
+		test('jsxFactory & jsxFragmentFactory', async () => {
+			const nodeProcess = await node.load('./src/tsx.tsx', {
+				cwd: './tsconfig',
+			});
+			expect(nodeProcess.stdout).toBe('div null hello world\nnull null goodbye world');
+		});
 
 		describe('paths', ({ test, describe }) => {
-			// test('resolves baseUrl', async () => {
-			// 	const nodeProcess = await node.load('./src/base-url.ts', {
-			// 		cwd: './tsconfig',
-			// 	});
-			// 	expect(nodeProcess.stdout).toBe('resolve-target');
-			// });
+			test('resolves baseUrl', async () => {
+				const nodeProcess = await node.load('./src/base-url.ts', {
+					cwd: './tsconfig',
+				});
+				expect(nodeProcess.stdout).toBe('resolve-target');
+			});
 
-			// test('resolves paths exact match', async () => {
-			// 	const nodeProcess = await node.load('./src/paths-exact-match.ts', {
-			// 		cwd: './tsconfig',
-			// 	});
-			// 	expect(nodeProcess.stdout).toBe('resolve-target');
-			// });
+			test('resolves paths exact match', async () => {
+				const nodeProcess = await node.load('./src/paths-exact-match.ts', {
+					cwd: './tsconfig',
+				});
+				expect(nodeProcess.stdout).toBe('resolve-target');
+			});
 
-			// test('resolves paths prefix', async () => {
-			// 	const nodeProcess = await node.load('./src/paths-prefix-match.ts', {
-			// 		cwd: './tsconfig',
-			// 	});
-			// 	expect(nodeProcess.stdout).toBe('nested-resolve-target');
-			// });
+			test('resolves paths prefix', async () => {
+				const nodeProcess = await node.load('./src/paths-prefix-match.ts', {
+					cwd: './tsconfig',
+				});
+				expect(nodeProcess.stdout).toBe('nested-resolve-target');
+			});
 
-			// test('resolves paths suffix', async () => {
-			// 	const nodeProcess = await node.load('./src/paths-suffix-match.ts', {
-			// 		cwd: './tsconfig',
-			// 	});
-			// 	expect(nodeProcess.stdout).toBe('nested-resolve-target');
-			// });
+			test('resolves paths suffix', async () => {
+				const nodeProcess = await node.load('./src/paths-suffix-match.ts', {
+					cwd: './tsconfig',
+				});
+				expect(nodeProcess.stdout).toBe('nested-resolve-target');
+			});
 
 			describe('dependency', ({ test }) => {
 				test('resolve current directory', async () => {
