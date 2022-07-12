699c91d70fa35f7403480148ede355f1843d178d
Hiroki Osame <hiroki.osame@gmail.com>
2022-07-12 16:17:29 -0400

    chore: use dev loaders

--- tests/utils/node-supports.ts
+++ tests/utils/node-supports.ts
@@ -1,7 +1,3 @@
 export default {
-	// esm: '>= 12.20.0',
-	// import: '^12.20.0 || >=13.2.0',
-	// sourceMap: '^14.18.0 || >=16.6.0',
-	// nodePrefixRequire: '^14.18.0 || >= 16.0.0',
 	testRunner: '>= 18.0.0',
 };
