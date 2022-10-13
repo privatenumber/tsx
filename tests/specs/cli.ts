ea1f52cf1f7393f94623a7ade44d63ecf6633b7a
Hiroki Osame <hiroki.osame@gmail.com>
2022-10-14 05:52:30 +0900

    wip

--- tests/specs/cli.ts
+++ tests/specs/cli.ts
@@ -5,55 +5,55 @@ import { tsx } from '../utils/tsx';
 
 export default testSuite(async ({ describe }, fixturePath: string) => {
 	describe('CLI', ({ describe }) => {
-		// describe('version', ({ test }) => {
-		// 	test('shows version', async () => {
-		// 		const tsxProcess = await tsx({
-		// 			args: ['--version'],
-		// 		});
+		describe('version', ({ test }) => {
+			test('shows version', async () => {
+				const tsxProcess = await tsx({
+					args: ['--version'],
+				});
 
-		// 		expect(tsxProcess.exitCode).toBe(0);
-		// 		expect(tsxProcess.stdout).toBe(packageJson.version);
-		// 		expect(tsxProcess.stderr).toBe('');
-		// 	});
+				expect(tsxProcess.exitCode).toBe(0);
+				expect(tsxProcess.stdout).toBe(packageJson.version);
+				expect(tsxProcess.stderr).toBe('');
+			});
 
-		// 	test('doesn\'t show version with file', async () => {
-		// 		const tsxProcess = await tsx({
-		// 			args: [
-		// 				path.join(fixturePath, 'log-argv.ts'),
-		// 				'--version',
-		// 			],
-		// 		});
+			test('doesn\'t show version with file', async () => {
+				const tsxProcess = await tsx({
+					args: [
+						path.join(fixturePath, 'log-argv.ts'),
+						'--version',
+					],
+				});
 
-		// 		expect(tsxProcess.exitCode).toBe(0);
-		// 		expect(tsxProcess.stdout).toMatch('"--version"');
-		// 		expect(tsxProcess.stderr).toBe('');
-		// 	});
-		// });
+				expect(tsxProcess.exitCode).toBe(0);
+				expect(tsxProcess.stdout).toMatch('"--version"');
+				expect(tsxProcess.stderr).toBe('');
+			});
+		});
 
-		// describe('help', ({ test }) => {
-		// 	test('shows help', async () => {
-		// 		const tsxProcess = await tsx({
-		// 			args: ['--help'],
-		// 		});
+		describe('help', ({ test }) => {
+			test('shows help', async () => {
+				const tsxProcess = await tsx({
+					args: ['--help'],
+				});
 
-		// 		expect(tsxProcess.exitCode).toBe(0);
-		// 		expect(tsxProcess.stdout).toMatch('Node.js runtime enhanced with esbuild for loading TypeScript & ESM');
-		// 		expect(tsxProcess.stderr).toBe('');
-		// 	});
+				expect(tsxProcess.exitCode).toBe(0);
+				expect(tsxProcess.stdout).toMatch('Node.js runtime enhanced with esbuild for loading TypeScript & ESM');
+				expect(tsxProcess.stderr).toBe('');
+			});
 
-		// 	test('doesn\'t show help with file', async () => {
-		// 		const tsxProcess = await tsx({
-		// 			args: [
-		// 				path.join(fixturePath, 'log-argv.ts'),
-		// 				'--help',
-		// 			],
-		// 		});
+			test('doesn\'t show help with file', async () => {
+				const tsxProcess = await tsx({
+					args: [
+						path.join(fixturePath, 'log-argv.ts'),
+						'--help',
+					],
+				});
 
-		// 		expect(tsxProcess.exitCode).toBe(0);
-		// 		expect(tsxProcess.stdout).toMatch('"--help"');
-		// 		expect(tsxProcess.stderr).toBe('');
-		// 	});
-		// });
+				expect(tsxProcess.exitCode).toBe(0);
+				expect(tsxProcess.stdout).toMatch('"--help"');
+				expect(tsxProcess.stderr).toBe('');
+			});
+		});
 
 		describe('Relays kill signal', async ({ test }) => {
 			const signals = ['SIGINT', 'SIGTERM'];
@@ -66,25 +66,18 @@ export default testSuite(async ({ describe }, fixturePath: string) => {
 						],
 					});
 
-					tsxProcess.stdout?.on('data', (data) => {
-						console.log('stdout', data.toString());
-					});
-
 					tsxProcess.stdout!.once('data', () => {
 						tsxProcess.kill(signal);
 					});
 
 					const tsxProcessResolved = await tsxProcess;
 
-					console.log(tsxProcessResolved);
-
+					expect(tsxProcessResolved.exitCode).toBe(200);
 					if (process.platform === 'win32') {
 						expect(tsxProcessResolved.stdout).toBe('READY');
 					} else {
 						expect(tsxProcessResolved.stdout).toBe(`READY\n${signal}\n${signal} HANDLER COMPLETED`);
 					}
-
-					expect(tsxProcessResolved.exitCode).toBe(200);
 				}, 5000);
 			}
 		});
