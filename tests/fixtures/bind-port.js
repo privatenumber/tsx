0a8822bf44adaf73d9c06edca54cebe6bea48def
Nikita Pavlov <n.pavlov@m-developer.ru>
2023-05-19 17:14:30 +0000

    test: child process overlap

--- tests/fixtures/bind-port.js
+++ tests/fixtures/bind-port.js
@@ -1,17 +1,17 @@
 process.on('SIGTERM', () => {
-    const waitTo = new Date() + 200;
-    // Long sync exit simulate
-    while (new Date() < waitTo) {}
-})
+	const waitTo = new Date() + 200;
+	// Long sync exit simulate
+	while (new Date() < waitTo) {}
+});
 import('net').then((net) => {
-    const server = net.createServer()
-    server.on('listening', () => {
-        console.log('READY');
-    })
-    server.listen(1024)
+	const server = net.createServer();
+	server.on('listening', () => {
+		console.log('READY');
+	});
+	server.listen(1024);
 });
 
 setTimeout(() => {
-    console.error('Forgotten process')
-    process.exit(1)
-}, 3000)
+	console.error('Forgotten process');
+	process.exit(1);
+}, 3000);
