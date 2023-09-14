7b8503c5be7b62619f607badfbe15b73d12f4728
Hiroki Osame <hiroki.osame@gmail.com>
2023-09-14 13:41:09 +0900

    test: refactor to resolve from cwd

--- tests/specs/json.ts
+++ tests/specs/json.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { testSuite, expect } from 'manten';
 import { createFixture } from 'fs-fixture';
 import type { NodeApis } from '../utils/tsx';
@@ -17,66 +16,61 @@ export default testSuite(async ({ describe }, node: NodeApis) => {
 		onFinish(async () => await fixture.rm());
 
 		describe('full path', async ({ test }) => {
-			const importPath = path.join(fixture.path, 'index.json');
 			test('Load', async () => {
-				const nodeProcess = await node.load(fixture.path);
+				const nodeProcess = await node.loadFile(fixture.path, './index.json');
 				expect(nodeProcess.stdout).toBe('');
 				expect(nodeProcess.exitCode).toBe(0);
 			});
 
 			test('Import', async () => {
-				const nodeProcess = await node.import(importPath);
-				expect(nodeProcess.stdout).toBe('{"default":{"loaded":"json"},"loaded":"json"}');
+				const nodeProcess = await node.importFile(fixture.path, './index.json');
+				expect(nodeProcess.stdout).toMatch('default: { loaded: \'json\' }');
 				expect(nodeProcess.stderr).toBe('');
 			});
 
 			test('Require', async () => {
-				const nodeProcess = await node.require(importPath);
-				expect(nodeProcess.stdout).toBe('{"loaded":"json"}');
+				const nodeProcess = await node.requireFile(fixture.path, './index.json');
+				expect(nodeProcess.stdout).toBe('{ loaded: \'json\' }');
 				expect(nodeProcess.stderr).toBe('');
 			});
 		});
 
 		describe('extensionless', ({ test }) => {
-			const importPath = path.join(fixture.path, 'index');
-
 			test('Load', async () => {
-				const nodeProcess = await node.load(importPath);
+				const nodeProcess = await node.loadFile(fixture.path, './index');
 				expect(nodeProcess.stdout).toBe('');
 				expect(nodeProcess.exitCode).toBe(0);
 			});
 
 			test('Import', async () => {
-				const nodeProcess = await node.import(importPath);
-				expect(nodeProcess.stdout).toBe('{"default":{"loaded":"json"},"loaded":"json"}');
+				const nodeProcess = await node.importFile(fixture.path, './index');
+				expect(nodeProcess.stdout).toMatch('default: { loaded: \'json\' }');
 				expect(nodeProcess.stderr).toBe('');
 			});
 
 			test('Require', async () => {
-				const nodeProcess = await node.require(importPath);
-				expect(nodeProcess.stdout).toBe('{"loaded":"json"}');
+				const nodeProcess = await node.requireFile(fixture.path, './index');
+				expect(nodeProcess.stdout).toBe('{ loaded: \'json\' }');
 				expect(nodeProcess.stderr).toBe('');
 			});
 		});
 
 		describe('directory', ({ test }) => {
-			const importPath = fixture.path;
-
 			test('Load', async () => {
-				const nodeProcess = await node.load(importPath);
+				const nodeProcess = await node.loadFile(fixture.path, '.');
 				expect(nodeProcess.stdout).toBe('');
 				expect(nodeProcess.exitCode).toBe(0);
 			});
 
 			test('Import', async () => {
-				const nodeProcess = await node.import(importPath);
-				expect(nodeProcess.stdout).toBe('{"default":{"loaded":"json"},"loaded":"json"}');
+				const nodeProcess = await node.importFile(fixture.path, '.');
+				expect(nodeProcess.stdout).toMatch('default: { loaded: \'json\' }');
 				expect(nodeProcess.stderr).toBe('');
 			});
 
 			test('Require', async () => {
-				const nodeProcess = await node.require(importPath);
-				expect(nodeProcess.stdout).toBe('{"loaded":"json"}');
+				const nodeProcess = await node.requireFile(fixture.path, '.');
+				expect(nodeProcess.stdout).toBe('{ loaded: \'json\' }');
 				expect(nodeProcess.stderr).toBe('');
 			});
 		});
