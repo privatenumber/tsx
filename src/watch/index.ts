40ebb48fd5b4c072747c8f3045e82b4f22f1bf67
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-10-05 16:17:42 -0400

    fix: stop assuming stdin is return key

--- src/watch/index.ts
+++ src/watch/index.ts
@@ -195,5 +195,11 @@ export const watchCommand = command({
 	).on('all', reRun);
 
 	// On "Return" key
-	process.stdin.on('data', reRun);
+	if (process.stdin.isTTY) {
+		process.stdin.on('data', (input) => {
+			if (input.toString() === '\n') {
+				reRun();
+			}
+		});
+	}
 });
