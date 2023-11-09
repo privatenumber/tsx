80cd7a7bd0399af0be438439a031c8f7540d6210
Hiroki Osame <hiroki.osame@gmail.com>
2023-11-09 14:46:45 +0900

    perf: strip source map when unused (#374)

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,36 +1,37 @@
-export const time = <Argument>(
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const time = <T extends (...args: any[]) => unknown>(
 	name: string,
-	_function: (...args: Argument[]) => unknown,
-) => function (
-		this: unknown,
-		...args: Argument[]
-	) {
-		const timeStart = Date.now();
-		const logTimeElapsed = () => {
-			const elapsed = Date.now() - timeStart;
+	_function: T,
+	threshold = 100,
+): T => function (
+	this: unknown,
+	...args: Parameters<T>
+) {
+	const timeStart = Date.now();
+	const logTimeElapsed = () => {
+		const elapsed = Date.now() - timeStart;
 
-			if (elapsed > 10) {
-			// console.log({
-			// 	name,
-			// 	args,
-			// 	elapsed,
-			// });
-			}
-		};
+		if (elapsed > threshold) {
+			console.log(name, {
+				args,
+				elapsed,
+			});
+		}
+	};
 
-		const result = Reflect.apply(_function, this, args);
-		if (
-			result
+	const result = Reflect.apply(_function, this, args);
+	if (
+		result
 		&& typeof result === 'object'
 		&& 'then' in result
-		) {
-			(result as Promise<unknown>).then(
-				logTimeElapsed,
-				// Ignore error in this chain
-				() => {},
-			);
-		} else {
-			logTimeElapsed();
-		}
-		return result;
-	};
+	) {
+		(result as Promise<unknown>).then(
+			logTimeElapsed,
+			// Ignore error in this chain
+			() => {},
+		);
+	} else {
+		logTimeElapsed();
+	}
+	return result;
+} as T;
