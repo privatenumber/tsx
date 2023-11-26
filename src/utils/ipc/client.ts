7d6c6fdeb3e09b433ba85cdd7821b8c1f1deffd1
Hiroki Osame <hiroki.osame@gmail.com>
2023-11-26 12:46:33 +0900

    wip

--- src/utils/ipc/client.ts
+++ src/utils/ipc/client.ts
@@ -4,22 +4,24 @@ import { getPipePath } from './get-pipe-path.js';
 export type SendToParent = (data: Record<string, unknown>) => void;
 
 // TODO: Handle when the loader is called directly
-const createIpcClient = () => new Promise<SendToParent>((resolve, reject) => {
+const createIpcClient = () => new Promise<SendToParent | void>((resolve) => {
 	const pipePath = getPipePath(process.ppid);
 	const socket: net.Socket = net.createConnection(
 		pipePath,
-		() => {
-			const send: SendToParent = (data) => {
-				const messageBuffer = Buffer.from(JSON.stringify(data));
-				const lengthBuffer = Buffer.alloc(4);
-				lengthBuffer.writeInt32BE(messageBuffer.length, 0);
-				socket.write(Buffer.concat([lengthBuffer, messageBuffer]));
-			};
-			resolve(send);
-		},
+		() => resolve((data) => {
+			const messageBuffer = Buffer.from(JSON.stringify(data));
+			const lengthBuffer = Buffer.alloc(4);
+			lengthBuffer.writeInt32BE(messageBuffer.length, 0);
+			socket.write(Buffer.concat([lengthBuffer, messageBuffer]));
+		}),
 	);
 
-	socket.on('error', reject);
+	/**
+	 * Ignore error:
+	 * - Called as a loader
+	 * - Nested process when using --test
+	 */
+	socket.on('error', () => resolve());
 
 	// Prevent Node from waiting for this socket to close before exiting
 	socket.unref();
