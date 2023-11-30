f4f19e0b87ba00a8bc3a701f2416ed307f9dfad4
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-11-30 11:08:11 +0900

    fix: adjust feature detection for node 18.19 loader changes (#422)
    Co-authored-by: Hiroki Osame <hiroki.osame@gmail.com>

--- src/utils/node-features.ts
+++ src/utils/node-features.ts
@@ -11,18 +11,21 @@ export const compareNodeVersion = (
 	|| nodeVersion[2] - version[2]
 );
 
-/**
- * Node.js loaders are isolated from v20
- * https://github.com/nodejs/node/issues/49455#issuecomment-1703812193
- * https://github.com/nodejs/node/blob/33710e7e7d39d19449a75911537d630349110a0c/doc/api/module.md#L375-L376
- */
-export const isolatedLoader = compareNodeVersion([20, 0, 0]) >= 0;
-
-export const supportsModuleRegister = compareNodeVersion([20, 6, 0]) >= 0;
+export const supportsModuleRegister = (
+	compareNodeVersion([20, 6, 0]) >= 0
+	|| (
+		compareNodeVersion([20, 0, 0]) < 0
+		&& compareNodeVersion([18, 19, 0]) >= 0
+	)
+);
 
 export const importAttributes = (
 	compareNodeVersion([21, 0, 0]) >= 0
 	|| compareNodeVersion([20, 10, 0]) >= 0
+	|| (
+		compareNodeVersion([20, 0, 0]) < 0
+		&& compareNodeVersion([18, 19, 0]) >= 0
+	)
 );
 
 // Added in v21.0.0
